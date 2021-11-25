76e9c45f6dcd99935f4fdfaaac16a91b53bce564
bandong-jpc <bandong.jpc@gmail.com>
2021-11-25 11:26:23 +0800

    add palette component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PaletteComponent } from './palette/palette.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PaletteComponent
   ],
   imports: [
     BrowserModule,
