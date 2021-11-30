50448fccb8b308e0f12420d1bf5b1f2efd6ee586
bandong-jpc <bandong.jpc@gmail.com>
2021-11-30 19:25:50 +0800

    add click to render element

--- src/app/palette/palette.component.ts
+++ src/app/palette/palette.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-palette',
   templateUrl: './palette.component.html',
-  styleUrls: ['./palette.component.css']
+  styleUrls: ['./palette.component.css'],
 })
 export class PaletteComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  createButton() {
+    const newButton = this.renderer.createElement('button');
+    const text = this.renderer.createText('BUTTON');
+    this.renderer.setProperty(newButton, 'type', 'button');
+    this.renderer.addClass(newButton, 'btn-primary');
+    this.renderer.appendChild(newButton, text);
+    this.renderer.appendChild(document.body, newButton);
+    return newButton;
   }
-
+  createLabel() {
+    const newLabel = this.renderer.createElement('p');
+    const text = this.renderer.createText('LABEL');
+    this.renderer.addClass(newLabel, 'h2');
+    this.renderer.appendChild(newLabel, text);
+    this.renderer.appendChild(document.body, newLabel);
+    return newLabel;
+  }
+  createInput() {
+    const newInput = this.renderer.createElement('input');
+    this.renderer.addClass(newInput, 'form-control');
+    this.renderer.appendChild(document.body, newInput);
+    this.renderer.setAttribute(newInput, 'placeholder', 'NEW INPUT');
+    return newInput;
+  }
 }
