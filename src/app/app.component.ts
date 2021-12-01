dd30898cc7650ca055bfded32d7cb4a108f4ff2b
bandong-jpc <bandong.jpc@gmail.com>
2021-12-01 11:44:55 +0800

    refactor component code into main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mockup-creator';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  createButton() {
+    const newButton = this.renderer.createElement('button');
+    const text = this.renderer.createText('BUTTON');
+    this.renderer.setProperty(newButton, 'type', 'button');
+    this.renderer.addClass(newButton, 'btn-primary');
+    this.renderer.appendChild(newButton, text);
+    this.renderer.appendChild(document.body, newButton);
+    return newButton;
+  }
+  createLabel() {
+    const newLabel = this.renderer.createElement('p');
+    const text = this.renderer.createText('LABEL');
+    this.renderer.addClass(newLabel, 'h2');
+    this.renderer.appendChild(newLabel, text);
+    this.renderer.appendChild(document.body, newLabel);
+    return newLabel;
+  }
+  createInput() {
+    const newInput = this.renderer.createElement('input');
+    this.renderer.addClass(newInput, 'form-control');
+    this.renderer.appendChild(document.body, newInput);
+    this.renderer.setAttribute(newInput, 'placeholder', 'NEW INPUT');
+    return newInput;
+  }
 }
