85b1f20229173ff48b045d976349dcd259b8191a
Juan Paolo Bandong <77478744+bandong-jpc@users.noreply.github.com>
2021-12-03 13:19:37 +0800

    Revert "add basic ui to project"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PaletteComponent } from './palette/palette.component';
-import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PaletteComponent,
-    CanvasComponent
+    PaletteComponent
   ],
   imports: [
     BrowserModule,
