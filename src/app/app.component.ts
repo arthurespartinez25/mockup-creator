bd294f0e9cb73d6abe4f504941efecc58c5b4294
sicatralph06 <sicatralph06@gmail.com>
2021-12-03 14:56:00 +0800

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,25 +47,29 @@ export class AppComponent implements OnInit {
     this.renderer.appendChild(this.canvas.nativeElement, newButton); //append the button to the canvas div
   }
   createCheckbox() {
+    const mainDiv = this.renderer.createElement('div'); 
     const newCheckbox = this.renderer.createElement('input'); 
     const checkboxLabel = this.renderer.createElement('label'); 
 
-    let ref2 = this.drag.createDrag(newCheckbox); 
-    let ref3 = this.drag.createDrag(checkboxLabel); 
-    ref2.withBoundaryElement(this.canvas); 
-    ref3.withBoundaryElement(this.canvas); 
+    this.renderer.appendChild(mainDiv, newCheckbox); 
+    this.renderer.appendChild(mainDiv, checkboxLabel); 
 
-    const text = this.renderer.createText('Checkbox'); 
+    let ref = this.drag.createDrag(mainDiv); 
+    ref.withBoundaryElement(this.canvas); 
+
+    this.renderer.addClass(mainDiv, 'form-check');
     this.renderer.addClass(newCheckbox, 'form-check-input');
-    this.renderer.addClass(checkboxLabel, 'form-check-label');
+    this.renderer.addClass(checkboxLabel, 'form-check-label');  
 
-    this.renderer.setProperty(newCheckbox, 'type', 'checkbox'); 
-    this.renderer.setProperty(newCheckbox, 'id', 'checkBoxID');
-    this.renderer.setAttribute(checkboxLabel, 'for', 'checkBoxID');
+    this.renderer.setProperty(newCheckbox, 'type', 'checkbox');
+    this.renderer.setProperty(newCheckbox, 'id', 'flexCheckDefault');
+    this.renderer.setProperty(newCheckbox, 'value', 'checkbox');
 
-    this.renderer.appendChild(checkboxLabel, text); 
-    this.renderer.appendChild(this.canvas.nativeElement, newCheckbox); 
-    this.renderer.appendChild(this.canvas.nativeElement, checkboxLabel); 
+    const text = this.renderer.createText('Label'); 
+    this.renderer.setAttribute(checkboxLabel, 'for', 'flexCheckDefault')
+    this.renderer.appendChild(checkboxLabel, text);
+
+    this.renderer.appendChild(this.canvas.nativeElement, mainDiv);
   }
   createLabel() {
     const checkboxLabel = this.renderer.createElement('label'); 
@@ -78,5 +82,57 @@ export class AppComponent implements OnInit {
 
     this.renderer.appendChild(checkboxLabel, text); 
     this.renderer.appendChild(this.canvas.nativeElement, checkboxLabel); 
+  }
+  
+  createDropdown() {
+    
+    const newDiv = this.renderer.createElement('div'); 
+    const drpButton = this.renderer.createElement('button'); 
+    const menuDiv = this.renderer.createElement('div'); 
+    const menu1 = this.renderer.createElement('a'); 
+    const menu2 = this.renderer.createElement('a'); 
+    const menu3 = this.renderer.createElement('a'); 
+    /* const firstDiv = this.renderer.insertBefore(newDiv, drpButton, this.canvas.nativeElement.firstChild); 
+    const secondDiv = this.renderer.insertBefore(newDiv, menuDiv, this.canvas.nativeElement.firstChild); */ 
+    this.renderer.appendChild(newDiv, drpButton); 
+    this.renderer.appendChild(newDiv, menuDiv); 
+    this.renderer.appendChild(menuDiv, menu1); 
+    this.renderer.appendChild(menuDiv, menu2); 
+    this.renderer.appendChild(menuDiv, menu3); 
+
+    let ref = this.drag.createDrag(drpButton); 
+    ref.withBoundaryElement(this.canvas); 
+
+    const text = this.renderer.createText('IbabaMoPo'); 
+    const menu1Text = this.renderer.createText('Menu1');
+    const menu2Text = this.renderer.createText('Menu2');
+    const menu3Text = this.renderer.createText('Menu3');
+
+    this.renderer.setProperty(drpButton, 'type', 'button');
+    this.renderer.setProperty(drpButton, 'id', 'dropdownMenuButton');
+    this.renderer.setProperty(menu1, 'href', '#');
+    this.renderer.setProperty(menu2, 'href', '#');
+    this.renderer.setProperty(menu3, 'href', '#');
+
+    this.renderer.addClass(newDiv, 'dropdown');
+    this.renderer.addClass(drpButton, 'btn');
+    this.renderer.addClass(drpButton, 'btn-secondary');
+    this.renderer.addClass(drpButton, 'dropdown-toggle');
+    this.renderer.addClass(menuDiv, 'dropdown-menu');
+    this.renderer.addClass(menu1, 'dropdown-item');
+    this.renderer.addClass(menu2, 'dropdown-item');
+    this.renderer.addClass(menu3, 'dropdown-item');
+    
+    this.renderer.setAttribute(drpButton, 'data-toggle', 'dropdown');
+    this.renderer.setAttribute(drpButton, 'aria-haspopup', 'true');
+    this.renderer.setAttribute(drpButton, 'aria-expanded', 'false');
+    this.renderer.setAttribute(menuDiv, 'aria-labelledby', 'dropdownMenuButton');
+
+    this.renderer.appendChild(drpButton, text); 
+    this.renderer.appendChild(menu1, menu1Text); 
+    this.renderer.appendChild(menu2, menu2Text); 
+    this.renderer.appendChild(menu3, menu3Text); 
+    this.renderer.appendChild(this.canvas.nativeElement, newDiv);
+
   }
 }
