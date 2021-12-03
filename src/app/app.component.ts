1c287895eae979c132c56ac23a5da7c9a4229471
Jude Bontigao <92580415+rjerbontigao123@users.noreply.github.com>
2021-12-03 15:28:46 +0800

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,19 +49,12 @@ export class AppComponent implements OnInit {
     //console.log(event.screenX);
     if (this.mousePosition.x === event.screenX && this.mousePosition.y === event.screenY){
     const newButton = this.renderer.createElement('button'); //create dom element
-
     let ref = this.drag.createDrag(newButton); //make the element draggable with createDrag, then store the reference to ref
-
     ref.withBoundaryElement(this.canvas); //set the draggable area to only be the canvas
-
     const text = this.renderer.createText('BUTTON'); //add text to button
-
     this.renderer.setProperty(newButton, 'type', 'button'); //add type attribute to button
-
     this.renderer.addClass(newButton, 'btn-primary'); //add css class to the button
-
     this.renderer.appendChild(newButton, text); //append the text into the button tag
-
     this.renderer.appendChild(this.canvas.nativeElement, newButton); //append the button to the canvas div
     }
   }
@@ -69,29 +62,46 @@ export class AppComponent implements OnInit {
    mouseUp($event: CdkDragEnd){
      
     console.log($event.source.getFreeDragPosition());
-    this.mousePosition2.x = $event.source.getFreeDragPosition().x;
-    this.mousePosition2.y = $event.source.getFreeDragPosition().y;
+    const { offsetLeft, offsetTop } = $event.source.element.nativeElement;
+    const { x, y } = $event.distance;
+    this.mousePosition2.x = offsetLeft + x;
+    this.mousePosition2.y = offsetTop + y;
    }
-  dragRelease( event: { screenX: number; screenY: number; }){
-    // console.log("Jude " +event.screenX);
-    // this.mousePosition2.x = event.screenX;
-    // this.mousePosition2.y = event.screenY;
-  }
-  onDragEnded(event: CdkDragEnd){
-    //console.log(this.mousePosition2.x);
+  
+  onDragEndedButton(event: CdkDragEnd){
     event.source._dragRef.reset();
     const newButton = this.renderer.createElement('button');
+    let ref = this.drag.createDrag(newButton);
+    ref.withBoundaryElement(this.canvas);
     const text = this.renderer.createText('BUTTON');
     this.renderer.setProperty(newButton, 'type', 'button');
     this.renderer.addClass(newButton, 'btn-primary');
     this.renderer.appendChild(newButton, text);
-    this.renderer.appendChild(document.body, newButton);
+    this.renderer.appendChild(this.canvas.nativeElement, newButton);
     this.renderer.setStyle(newButton, 'position', 'absolute');
     this.renderer.setStyle(newButton, 'left', this.mousePosition2.x+ "px");
-    this.renderer.setStyle(newButton, 'bottom', this.mousePosition2.y + "px");
+    this.renderer.setStyle(newButton, 'top', this.mousePosition2.y + "px");
     
     return newButton;
     
+}
+// other buttons
+onDragEnded(event: CdkDragEnd){
+  event.source._dragRef.reset();
+  const newButton = this.renderer.createElement('button');
+  let ref = this.drag.createDrag(newButton);
+  ref.withBoundaryElement(this.canvas);
+  const text = this.renderer.createText('BUTTON');
+  this.renderer.setProperty(newButton, 'type', 'button');
+  this.renderer.addClass(newButton, 'btn-primary');
+  this.renderer.appendChild(newButton, text);
+  this.renderer.appendChild(this.canvas.nativeElement, newButton);
+  this.renderer.setStyle(newButton, 'position', 'absolute');
+  this.renderer.setStyle(newButton, 'left', this.mousePosition2.x+ "px");
+  this.renderer.setStyle(newButton, 'top', this.mousePosition2.y + "px");
+  
+  return newButton;
+  
 }
 
   
