a717a26b069b54cb48bfb73f6eb277fe961ad5ea
bandong-jpc <bandong.jpc@gmail.com>
2021-12-03 15:44:06 +0800

    fix navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,16 +30,39 @@ export class AppComponent implements OnInit {
   }
 
   createNav() {
-    const newElem = this.renderer.createElement('div'); //create dom element
-
-    let ref = this.drag.createDrag(newElem); //make the element draggable with createDrag, then store the reference to ref
-
+    const newNav = this.renderer.createElement('div'); //create dom element
+    let ref = this.drag.createDrag(newNav); //make the element draggable with createDrag, then store the reference to ref
     ref.withBoundaryElement(this.canvas); //set the draggable area to only be the canvas
 
-    this.renderer.addClass(newElem, 'navbar'); //add css class to the button
-    this.renderer.addClass(newElem, 'custom-nav'); //add css class to the button
+    const newLabel = this.renderer.createElement('h1');
+    const text = this.renderer.createText('NAV');
 
-    this.renderer.appendChild(this.canvas.nativeElement, newElem); //append the button to the canvas div
+    const link1 = this.renderer.createElement('a');
+    const link2 = this.renderer.createElement('a');
+    const link3 = this.renderer.createElement('a');
+
+    this.renderer.appendChild(link1, this.renderer.createText('LINK1'));
+    this.renderer.appendChild(link2, this.renderer.createText('LINK2'));
+    this.renderer.appendChild(link3, this.renderer.createText('LINK3'));
+
+    this.renderer.setProperty(link1, 'href', '#');
+    this.renderer.setProperty(link2, 'href', '#');
+    this.renderer.setProperty(link3, 'href', '#');
+
+    this.renderer.appendChild(newLabel, text);
+    this.renderer.appendChild(newNav, newLabel);
+    this.renderer.appendChild(newNav, link1);
+    this.renderer.appendChild(newNav, link2);
+    this.renderer.appendChild(newNav, link3);
+
+    this.renderer.addClass(link1, 'custom-nav-link');
+    this.renderer.addClass(link2, 'custom-nav-link');
+    this.renderer.addClass(link3, 'custom-nav-link');
+
+    this.renderer.addClass(newNav, 'navbar'); //add css class to the button
+    this.renderer.addClass(newNav, 'custom-nav'); //add css class to the button
+
+    this.renderer.appendChild(this.canvas.nativeElement, newNav); //append the button to the canvas div
   }
 
   createButton() {
