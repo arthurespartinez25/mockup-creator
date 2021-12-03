f980d54be7ce8ae06e1d384b2a8261c8aa08f22a
ProgramMurl <50584275+ProgramMurl@users.noreply.github.com>
2021-12-03 10:34:12 +0800

    Add canvas to print canvas html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,11 @@ import {
 })
 export class AppComponent implements OnInit {
   title = 'mockup-creator';
-  //buttonPrint = `<button _ngcontent-rfn-c16="" type="button" class="btn-primary">BUTTON</button>`;
-  //labelPrint = `<p _ngcontent-rfn-c16="" class="h2">LABEL</p>`;
-  //textFieldPrint = `<input _ngcontent-rfn-c16="" class="form-control" placeholder="NEW INPUT">`;
+  //htmlCode =  '<html><body>';
   //flag = 0;
 
   @ViewChild('canvas') canvas!: ElementRef;
+  @ViewChild('canvashtml') htmlCanvas!: ElementRef;
 
   constructor(private renderer: Renderer2) {}
 
@@ -33,8 +32,7 @@ export class AppComponent implements OnInit {
     this.renderer.addClass(newButton, 'btn-primary');
     this.renderer.appendChild(newButton, text);
     this.renderer.appendChild(this.canvas.nativeElement, newButton);
-    //this.flag = 1;
-    console.log(newButton);
+    //console.log(newButton);
     return newButton;
   }
   createLabel() {
@@ -43,8 +41,7 @@ export class AppComponent implements OnInit {
     this.renderer.addClass(newLabel, 'h2');
     this.renderer.appendChild(newLabel, text);
     this.renderer.appendChild(this.canvas.nativeElement, newLabel);
-    //this.flag = 2;
-    console.log(newLabel);
+    //console.log(newLabel);
     return newLabel;
   }
   createInput() {
@@ -52,15 +49,19 @@ export class AppComponent implements OnInit {
     this.renderer.addClass(newInput, 'form-control');
     this.renderer.appendChild(this.canvas.nativeElement, newInput);
     this.renderer.setAttribute(newInput, 'placeholder', 'NEW INPUT');
-    //this.flag = 3;
-    console.log(newInput);
+    //console.log(newInput);
     return newInput;
   }
 
-  /*print(): void {
-     if (this.flag === 1){console.log(this.buttonPrint);}
+  print(): void {
+     /*if (this.flag === 1){console.log(this.buttonPrint);}
      else if (this.flag === 2){console.log(this.textFieldPrint);}
      else if (this.flag === 3){console.log(this.labelPrint);}
-     this.flag = 0;
-  }*/
+     this.flag = 0;*/
+     const htmlCode = this.canvas.nativeElement.outerHTML;
+     const text = this.renderer.createText(htmlCode);
+     this.renderer.appendChild(this.htmlCanvas.nativeElement, text);
+     console.log(htmlCode);
+     console.log(text);
+  }
 }
