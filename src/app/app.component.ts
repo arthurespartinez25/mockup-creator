1cd6680866fc967f263de8b6f194a2f83536e7f9
bandong-jpc <bandong.jpc@gmail.com>
2021-12-03 10:37:28 +0800

    change UI colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,12 +31,19 @@ export class AppComponent implements OnInit {
 
   createButton() {
     const newButton = this.renderer.createElement('button'); //create dom element
+
     let ref = this.drag.createDrag(newButton); //make the element draggable with createDrag, then store the reference to ref
+
     ref.withBoundaryElement(this.canvas); //set the draggable area to only be the canvas
+
     const text = this.renderer.createText('BUTTON'); //add text to button
+
     this.renderer.setProperty(newButton, 'type', 'button'); //add type attribute to button
+
     this.renderer.addClass(newButton, 'btn-primary'); //add css class to the button
+
     this.renderer.appendChild(newButton, text); //append the text into the button tag
+
     this.renderer.appendChild(this.canvas.nativeElement, newButton); //append the button to the canvas div
   }
 }
