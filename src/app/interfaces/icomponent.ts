759fce124f6586f219e104ac5aa5978d23e043bc
MikmikMikmikMikmik <92725990+MikmikMikmikMikmik@users.noreply.github.com>
2021-12-15 16:27:59 +0800

    deleting contents

--- src/app/interfaces/icomponent.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef } from '@angular/core';
-import { IProperty } from './iproperty';
-
-export interface IComponent {
-  canvas: ElementRef;
-  props: IProperty;
-  get htmlCode(): string;
-}
