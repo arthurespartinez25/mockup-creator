610daca12a4b40755313614d5d4efb5f33738049
Jude Bontigao <92580415+rjerbontigao123@users.noreply.github.com>
2022-01-10 16:20:06 +0800

    bug fix desu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -86,6 +86,7 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   @ViewChild('PropertyComponent') property: boolean;
   @ViewChild('canvas') canvas!: ElementRef;
+  @ViewChild('textOp') textBtn!: ElementRef;
 
   constructor(private renderer: Renderer2, private drag: DragDrop) {}
   delete: boolean;
@@ -107,6 +108,7 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
   }
 
   addComponent(component: string) {
+    console.log("luv u kuya mik");
     let temp: IComponent;
     switch (component) {
       case 'nav':
@@ -195,6 +197,17 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
     this.mousePositionX = offsetLeft + x;
     this.mousePositionY = offsetTop + y;
     
+  }
+  onDragEndedText(event: CdkDragEnd) {
+    event.source._dragRef.reset();
+    const { offsetLeft, offsetTop } = event.source.element.nativeElement;
+    const { x, y } = event.distance;
+    let canvasLeftX = (this.textBtn.nativeElement as HTMLElement).offsetWidth;
+    let canvasTopY = (this.textBtn.nativeElement as HTMLElement).offsetTop;
+    this.mousePositionX = offsetLeft + x + canvasLeftX;
+    this.mousePositionY = offsetTop + y + canvasTopY;
+    console.log(x);
+    console.log(this.canvasLeft);
   }
 
   onDragEndedAddComponent(component: string) {
