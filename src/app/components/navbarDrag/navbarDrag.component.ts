d59d98ecb48c44a9560c9da63d28b7c7243fd781
jameswilmer <jameswilmerlim@gmail.com>
2022-01-14 13:26:10 +0800

    Changes to navbar

--- src/app/components/navbarDrag/navbarDrag.component.ts
+++ src/app/components/navbarDrag/navbarDrag.component.ts
@@ -22,7 +22,7 @@ export class NavbarDragComponent implements OnInit, IComponent {
     typeObj: 'navDrag',
     type: '',
   };
-  
+
   @Input() canvasWW: any;
   @Output() updateDataEvent= new EventEmitter<any>();
   @Output() updateDataEventY= new EventEmitter<any>();
@@ -47,7 +47,7 @@ export class NavbarDragComponent implements OnInit, IComponent {
     this.theCanvasWidth = this.canvasWW;
     this.dagaX = this.xmouse;
     this.dagaY = this.ymouse;
-    let percentageX = ((this.xmouse-this.theX)/1280)*100; 
+    let percentageX = ((this.xmouse-this.theX)/1280)*100;
     let percentageY = ((this.ymouse-this.theY)/720)*100;
     if(this.whatComponent2 == "searchNavbar")
     {
@@ -57,15 +57,14 @@ export class NavbarDragComponent implements OnInit, IComponent {
     }
     else
     {
-      this.props.style='width: 100%; color: white;padding: 10px;background-color: #12355B;font-size: 20px;left:'
-    +percentageX+'%;top:'+percentageY+'%;';
+      this.props.style='position:absolute;width: 100%; color: white;padding: 10px;background-color: #12355B;font-size: 20px;left:0%;top:'+percentageY+'%;';
     }
   }
 
   onDragEnded($event: any){
     this.mousePositionXV2 = $event.source.getFreeDragPosition().x;
     this.mousePositionYV2 = $event.source.getFreeDragPosition().y;
-    
+
     this.updateDataEvent.emit(0);
     this.updateDataEventY.emit(((this.mousePositionYV2 + this.dagaY - this.theY)/720)*100);
     console.log(this.theCanvasWidth);
