5b07341767be90709fc393bff98b098b27191f16
Jude Bontigao <92580415+rjerbontigao123@users.noreply.github.com>
2022-01-24 10:47:25 +0800

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,6 +93,7 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
   @ViewChild('canvas') canvas!: ElementRef;
   //@ViewChild('textOp') textBtn!: ElementRef;
   @ViewChild('subMenuItem') subMenuItem!: ElementRef;
+  @ViewChild('subMenuItem2') subMenuItem2!: ElementRef;
 
   changeref: ChangeDetectorRef;
   constructor(
@@ -239,6 +240,11 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
           this.canvasLeftX = 0;
           this.canvasTopY = 0;
         }
+        else if(component == 'header'||component == 'paragraph'||component == 'label')
+        {
+          this.canvasLeftX = (this.subMenuItem2.nativeElement as HTMLElement).offsetWidth;
+          this.canvasTopY = (this.subMenuItem2.nativeElement as HTMLElement).offsetTop;
+        }
         this.mousePositionX = this.offsetLeft + this.xDis + this.canvasLeftX;
         this.mousePositionY = this.offsetTop + this.yDis + this.canvasTopY;
       }
@@ -325,6 +331,13 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
     this.changeref.detectChanges();
   }
   jude = 'aw';
+  isDisabled = true;
+  timerDisable()
+  {
+    setTimeout(() => {
+      this.isDisabled = true;
+    }, 100);
+  }
   clickHandler(component: IComponent) {
     this.selected = component.props;
     this.selectedComponent = component;
