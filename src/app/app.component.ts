adc8c3bc0a1deb7e0e2ec73f6ae37888ee99a458
Jude Bontigao <92580415+rjerbontigao123@users.noreply.github.com>
2022-02-03 15:55:58 +0800

    japanese

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,7 +80,7 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
     '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>';
   private _styleBody4 =
     '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>';
-  private _htmlStart = '<!doctype html>\n<html lang="en">';
+  private _htmlStart = '<!doctype html>\n<html lang="en">\n<meta charset="utf-8">';
   private _htmlEnd = '</html>';
   private _bootstrapLink =
     '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">';
