909afba351491e8a8d6a04f963892fcc36e8e973
Jesu Lacastesantos <95595805+Iyesu@users.noreply.github.com>
2022-02-22 14:50:43 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -267,21 +267,6 @@ export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
   onDragEnd(component: IComponent) {
     console.log(component);
   }
-
-  copyMessage(val: string) {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
-    selBox.value = val;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    document.execCommand('copy');
-    document.body.removeChild(selBox);
-  }
-
   refresh(): void {
     this._router
       .navigateByUrl('/refresh', { skipLocationChange: true })
